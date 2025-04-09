bbd07be6657f29c8823d11699c8bb873e138c25a
Subhankar <subhankaritzme@gmail.com>
2025-04-09 21:09:15 +0530

    minHeight

--- GFG/MinHeightsII/MinHeightsII.java
+++ GFG/MinHeightsII/MinHeightsII.java
@@ -20,23 +20,16 @@ public class MinHeightsII{
 
 class Solution {
     int getMinDiff(int[] arr, int k) {
-        
-        int [] min_arr = {Integer.MAX_VALUE,Integer.MAX_VALUE};
-        int [] max_arr = {Integer.MIN_VALUE,Integer.MIN_VALUE};
+        Arrays.sort(arr);
+        int n = arr.length;
+        int minDiff = arr[n-1]-arr[0]; 
+        for(int i=1;i<n;i++){
+        	if (arr[i] - k < 0)
+            	continue;
+        	int minTower = Math.min(arr[0]+k,arr[i]-k);
+        	int maxTower = Math.max(arr[i-1]+k,arr[n-1]-k);
 
-        for(int ele : arr){
-        	min_arr[0] = Math.min(min_arr[0],ele+k);
-        	min_arr[1] = ele-k<0 ? min_arr[1]:Math.min(min_arr[1],ele-k);
-
-        	max_arr[0] = Math.max(max_arr[0],ele+k);
-        	max_arr[1] = ele-k<0 ? max_arr[1]:Math.max(max_arr[1],ele-k);
-        }
-
-        int minDiff = Integer.MAX_VALUE;
-        for(int min:min_arr){
-        	for(int max : max_arr){
-        		minDiff = max-min<0 ? minDiff:Math.min(minDiff,(max-min));
-        	}
+        	minDiff = Math.min(minDiff,maxTower-minTower);
         }
         return minDiff;
     }
